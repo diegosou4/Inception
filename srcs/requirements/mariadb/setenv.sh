72923b37a41705329388df3f788d754923d69fbd
diegmore <diegmore@Inception.Inception.virtualbox.org>
2024-12-08 15:12:28 +0000

    (fix) i need fix

--- srcs/requirements/mariadb/setenv.sh
+++ srcs/requirements/mariadb/setenv.sh
@@ -1,3 +1,29 @@
-export MYSQL_PASSWORD=$(cat /run/secrets/db_password)
-export MYSQL_ROOT_PASSWORD=$(cat /run/secrets/db_root_password)
-exec "$@"
+
+
+
+MYSQL_PASSWORD=$(cat "$MYSQL_PASSWORD_FILE")
+MYSQL_ROOT_PASSWORD=$(cat "$MYSQL_ROOT_PASSWORD_FILE")
+
+
+if [ ! -f /var/lib/mysql/.initialized ]; then
+  # Comandos de configuração
+  echo "Configurando MariaDB..."
+  service mariadb restart
+
+  mysql -u root -e "
+  CREATE USER IF NOT EXISTS 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
+  FLUSH PRIVILEGES;
+  CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
+  CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
+  GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';
+  FLUSH PRIVILEGES;
+  SHUTDOWN;
+  "
+  touch /var/lib/mysql/.initialized
+else
+  echo "MariaDB já foi inicializado."
+fi
+
+echo "Iniciando MariaDB..."
+
+mysqld_safe --bind-address=0.0.0.0 --port=3306
